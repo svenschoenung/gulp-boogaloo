fdc468041417e5b19a917d5a60795ed15bcb35e6
Sven Schoenung <sven.schoenung@gmail.com>
2016-05-24 21:55:59 +0200

    Remove unused code

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-var extend = require('extend');
-var util = require('util');
-
 var task = require('./lib/task.js');
 var series = require('./lib/series.js');
 var parallel = require('./lib/parallel.js');
 
 module.exports = function(gulp, opts) {
 
-  var options = extend({}, {
-  }, opts);
- 
   var g = Object.create(gulp);
   g.task = function() {
     return task(gulp, arguments);
